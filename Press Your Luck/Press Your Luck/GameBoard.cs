6f32d7f5bc334b09ff324f77e6bc18f58118818a
Nathan Durst <nathandurst3@gmail.com>
2017-05-03 11:04:03 -0500

    Update GameBoard.cs
    remove commented code

--- Press Your Luck/Press Your Luck/GameBoard.cs
+++ Press Your Luck/Press Your Luck/GameBoard.cs
@@ -25,26 +25,7 @@ namespace Press_Your_Luck
         {
             InitializeComponent();
             tile = Controls.OfType<PictureBox>().ToArray();
-            /*image[0] = Properties.Resources._500;
-            image[1] = Properties.Resources._500spin;
-            image[2] = Properties.Resources._530;
-            image[3] = Properties.Resources._650;
-            image[4] = Properties.Resources._750;
-            image[5] = Properties.Resources._750spin;
-            image[6] = Properties.Resources._800;
-            image[7] = Properties.Resources._900;
-            image[8] = Properties.Resources._1000;
-            image[9] = Properties.Resources._1000s;
-            image[10] = Properties.Resources._1100;
-            image[11] = Properties.Resources._1200;
-            image[12] = Properties.Resources._1250;
-            image[13] = Properties.Resources._1300;
-            image[14] = Properties.Resources._1400;
-            image[15] = Properties.Resources._1500;
-            image[16] = Properties.Resources._1500s;
-            image[17] = Properties.Resources.whammy1;
-            image[18] = Properties.Resources.whammy2;
-            image[19] = Properties.Resources.whammy3;*/
+            
             image = new Bitmap[] {Properties.Resources._500,
                 Properties.Resources._500spin, Properties.Resources._530,
                 Properties.Resources._650, Properties.Resources._750,
@@ -74,22 +55,6 @@ namespace Press_Your_Luck
             
             for (int i = 0; i < 14; i++)
                 tile[i].Image = image[i];
-            /*for (int i = 0; i < 5; i++)
-            {
-                questionLabel.Text = question from file
-                read key from keyboard
-                read answer from answerTextBox
-                if key = 'a'
-                    currentPlayer = playerOne
-                else
-                    currentPlayer = playerTwo
-                if answerTextBox == answer (read from file)
-                    currentPlayer.spins+=3
-                    questionLabel.Text = "CORRECT. +3 SPINS"
-                else
-                    currentPlayer.spins--
-                    questionLabel.Text = "INCORRECT. -1 SPIN"
-            }*/
         }
 
         private void spinButton_Click(object sender, EventArgs e)
